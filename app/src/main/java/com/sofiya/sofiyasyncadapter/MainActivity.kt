a5e2d011ac6ebfc27e59272230ff510f6c8b98c4
Sofiya Taskova <sofiya.taskova@reddit.com>
2020-09-21 16:30:06 -0700

    immitate the release app account type

--- app/src/main/java/com/sofiya/sofiyasyncadapter/MainActivity.kt
+++ app/src/main/java/com/sofiya/sofiyasyncadapter/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val accountType = "com.sofiya.account"
+        val accountType = "com.reddit.account"
 
         val am = AccountManager.get(this) // "this" references the current Context
         val accounts = am.getAccountsByType(accountType)
